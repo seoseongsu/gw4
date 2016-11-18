562e498ded7de4eb1d816ee7af654b648565e942
user2 <user2@user2-PC>
2016-11-18 17:32:06 +0900

    main 패키지 추가 , main(하위항목포함) 폴더 추가

---
+++ gw4/src/gw/main/MainController.java
@@ -0,0 +1,74 @@
+package gw.main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import gw.employee.EmployeeVO;
+@Controller
+public class MainController{
+	@Autowired
+	SqlMapClientTemplate sqlMap;
+	public static ArrayList accessUserCount = new ArrayList();
+	
+	@RequestMapping("/main/main.do")
+	public String main(HttpServletRequest request, HttpSession session){	 
+		String emp_code = (String)session.getAttribute("memId");
+		
+		EmployeeVO dto = (EmployeeVO)sqlMap.queryForObject("empSelect", emp_code);
+		request.setAttribute("empList", dto);
+		request.setAttribute("emp_code", emp_code);
+		return"/main/main.jsp";
+	}
+	
+	@RequestMapping("/main/loginPro.do")
+	public String loginPro(EmployeeVO dto, HttpSession session, HttpServletRequest request){
+		ServletContext application = request.getServletContext();
+		HashMap userInfo = new HashMap();
+		int check = (Integer)sqlMap.queryForObject("userCheck", dto);
+		String x = "";
+		if(check == 1){
+			session.setAttribute("memId", dto.getEmp_code());
+			application.setAttribute("appId", dto.getEmp_code());
+			String id = (String)application.getAttribute("appId");
+			
+			dto = (EmployeeVO)sqlMap.queryForObject("getEmpList",id);
+			
+			String ec = (String)sqlMap.queryForObject("codeCheck", dto.getEmp_code());
+			session.setAttribute("empCode", ec);
+			String name = (String)sqlMap.queryForObject("selectNm", dto.getEmp_name());
+			session.setAttribute("empName", name);
+			
+			userInfo.put(session, session.getId());
+			userInfo.put("empcode", dto.getEmp_code());
+			userInfo.put("empName", dto.getEmp_name());
+			
+			accessUserCount.add(userInfo);
+			
+			application.setAttribute("userlist", accessUserCount);
+			
+			x = "redirect:/main/main.nhn";
+		}else{
+			request.setAttribute("check", new Integer(check));
+			x = "main/loginPro.jsp";
+		}
+		return x;
+	}
+	@RequestMapping("/main/logout.do")
+	public String logout(){
+		return "/main/logout.jsp";
+	}
+	@RequestMapping("/error/error.do")
+	public String error(){
+		return "/error/error.jsp";
+	}
+}
+
