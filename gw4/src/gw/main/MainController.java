8632d5522155910b84be0c81500c83f5eb1e6525
user2 <user2@user2-PC>
2016-11-18 20:39:07 +0900

    main

--- gw4/src/gw/main/MainController.java
+++ gw4/src/gw/main/MainController.java
@@ -20,55 +20,30 @@ public class MainController{
 	public static ArrayList accessUserCount = new ArrayList();
 	
 	@RequestMapping("/main/main.do")
-	public String main(HttpServletRequest request, HttpSession session){	 
-		String emp_code = (String)session.getAttribute("memId");
+	public String main(){	 
+
+		return"/main/main";
+	}
+	
+	@RequestMapping("/main/login.do")
+	public String login(){
 		
-		EmployeeVO dto = (EmployeeVO)sqlMap.queryForObject("empSelect", emp_code);
-		request.setAttribute("empDto", dto);
-		request.setAttribute("empCode", emp_code);
-		return"/main/main.jsp";
+		return "/main/login";
 	}
 	
 	@RequestMapping("/main/loginPro.do")
-	public String loginPro(EmployeeVO dto, HttpSession session, HttpServletRequest request){
-		ServletContext application = request.getServletContext();
-		HashMap userInfo = new HashMap();
-		int check = (Integer)sqlMap.queryForObject("userCheck", dto);
-		String x = "";
-		if(check == 1){
-			session.setAttribute("memId", dto.getEmp_code());
-			application.setAttribute("appId", dto.getEmp_code());
-			String id = (String)application.getAttribute("appId");
-			
-			dto = (EmployeeVO)sqlMap.queryForObject("getEmpList",id);
-			
-			String ec = (String)sqlMap.queryForObject("codeCheck", dto.getEmp_code());
-			session.setAttribute("empCode", ec);
-			String name = (String)sqlMap.queryForObject("selectNm", dto.getEmp_name());
-			session.setAttribute("empName", name);
-			
-			userInfo.put(session, session.getId());
-			userInfo.put("empcode", dto.getEmp_code());
-			userInfo.put("empName", dto.getEmp_name());
-			
-			accessUserCount.add(userInfo);
-			
-			application.setAttribute("userlist", accessUserCount);
-			
-			x = "redirect:/main/main.nhn";
-		}else{
-			request.setAttribute("check", new Integer(check));
-			x = "main/loginPro.jsp";
-		}
-		return x;
+	public String loginPro(){
+		
+		return "/main/loginPro";
 	}
+	
 	@RequestMapping("/main/logout.do")
 	public String logout(){
-		return "/main/logout.jsp";
+		return "/main/logout";
 	}
 	@RequestMapping("/error/error.do")
 	public String error(){
-		return "/error/error.jsp";
+		return "/error/error";
 	}
 }
 
