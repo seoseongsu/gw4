6dac331b57781ec7ac5b1b9a350a6f73f120cc45
user2 <user2@user2-PC>
2016-11-22 19:44:00 +0900

    업무명세서시작

---
+++ gw4/src/gw/bstatement/BstatementController.java
@@ -0,0 +1,21 @@
+package gw.bstatement;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BstatementController {
+	@Autowired
+	private SqlMapClientTemplate sqlMap;
+	
+	@RequestMapping("/bs/bsList.do")
+	public String bsList(HttpServletRequest request){
+		
+		return "/bs/bsList";
+	}
+}
---
+++ gw4/src/gw/bstatement/BstatementVO.java
@@ -0,0 +1,5 @@
+package gw.bstatement;
+
+public class BstatementVO {
+
+}
