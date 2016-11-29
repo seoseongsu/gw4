f47e934642f3b4def28599fa5a5b44a7f3b2f0ab
user2 <user2@user2-PC>
2016-11-29 16:55:03 +0900

    .

--- gw4/src/gw/commute/CommuteController.java
+++ gw4/src/gw/commute/CommuteController.java
@@ -1,5 +1,19 @@
 package gw.commute;
 
-public class CommuteController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommuteController {
+	@Autowired
+	SqlMapClientTemplate sqlMap;
+	@RequestMapping("/commute/comList.do")
+	public String comList(HttpServletRequest request){
+		
+		return "/commute/comList.do";
+	}
 }
